999840e36cc53fe73b78cfe63cb633f0d085290a
FabricioLucchetta <164436840+FabricioLucchetta@users.noreply.github.com>
2024-09-27 15:36:31 -0300

    Add files via upload

---
+++ index.html
@@ -0,0 +1,18 @@
+<?php include("template/cabecera.php"); ?>
+    
+
+
+<div class="jumbotron text-center">
+    <h1 class="display-3">Bienvenidos al sitio de libros</h1>
+    <p class="lead">Consulta libros de programación</p>
+    <hr class="my-2">
+
+<img width="700" src="img/libros.jpg" class="img-thumbnail rounded" />
+
+    <p>Más Información</p>
+
+        <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver almacen de libros</a>
+  
+</div>
+
+<?php include("template/pie.php"); ?>
---
+++ nosotros.php
@@ -0,0 +1,32 @@
+<?php include("template/cabecera.php"); ?>
+    
+
+<div class="col-md-6">
+    <h1 class="display-3">Nosotros</h1>
+    <p class="lead">Hola somos un grupo de personas que se dedican al desarrollo web.
+        Diseñamos Tu Web a Medida - Planeamiento Estratégico y Diseño de sitios web a medida para tu empresa. ¡Consultá ahora! Diseñamos una página web para maximizar el rendimiento de tu marca.
+        Posicionamiento Web en principales buscadores, gestion en redes sociales, email marketing. Alcanza todos tus Alcanza todos tus objetivos en un solo lugar.
+        <br> Programming Notes for Professionals books
+    </p>
+    <hr>
+
+</div>
+
+<div class="col-md-6">
+    <h1 class="display-2">Contacto</h1>
+    <p class="lead">Redes Sociales: Facebook, Whatsapp, Instagram</p>
+    <img width="315" src="img/redes.jpg"/>
+    <hr>
+
+</div>
+
+<div class="col-md-6">
+    <h1 class="display-3">Ubicación</h1>
+    <p class="lead">Google Maps</p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22066.780297510155!2d-58.51152641020499!3d-34.69896169462736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccec2b3ab2337%3A0x414fcc823d6d06a5!2sTapiales%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1726512740334!5m2!1ses-419!2sar" width="600" height="450"></iframe>
+</div>
+
+
+
+
+<?php include("template/pie.php"); ?>
